284d882dadaddc110ae171fc71e333e335e9a618
Ajay_00 <ajaylohith855@gmail.com>
2026-04-07 00:31:58 +0530

    Email fix1

--- backend/src/main/java/com/placementportal/config/MailConfig.java
+++ backend/src/main/java/com/placementportal/config/MailConfig.java
@@ -13,10 +13,10 @@ public class MailConfig {
 
     @Bean
     public JavaMailSender javaMailSender(
-            @Value("${spring.mail.host:smtp.gmail.com}") String host,
-            @Value("${spring.mail.port:587}") int port,
-            @Value("${spring.mail.username:}") String username,
-            @Value("${spring.mail.password:}") String password) {
+            @Value("${spring.mail.host}") String host,
+            @Value("${spring.mail.port}") int port,
+            @Value("${spring.mail.username}") String username,
+            @Value("${spring.mail.password}") String password) {
 
         JavaMailSenderImpl sender = new JavaMailSenderImpl();
         sender.setHost(host);
